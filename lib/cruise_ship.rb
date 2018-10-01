763565a82c57332063f9698bcc17b0adeb8a75a6
Lola Onanuga <slonanuga@gmail.com>
2018-10-01 15:16:33 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,7 +9,7 @@ suite_e: "Crumpet the Elf"
 def select_winner(passengers)
 passengers.collect do |suite, name|
   if suite == :suite_a && 
-    name.start_with("A")
+    name.start_with?("A")
       winner = name
     end
 end
